6fdd7286d59ebb1974f18e2869a6a7d133dfc1b7
Lisa Pääjärvi <65873813+lisapaajarvi@users.noreply.github.com>
2021-04-22 08:39:46 +0200

    express async errors

--- server.js
+++ server.js
@@ -6,8 +6,16 @@ app.get('/', (req,res) =>
     res.send('Hello World')
 )
 
+async function run() {
+    try { 
+        await mongoose.connect('mongodb://localhost:27017/art', {useNewUrlParser: true, useUnifiedTopology: true});
+        console.log('Database is connected');
+    } catch (error) {
+        console.error(error)
+    }
+}
 
-mongoose.connect('mongodb://localhost/art')
+run();
 
 app.listen(3000, ()=> (
     console.log('Server is up and running')
